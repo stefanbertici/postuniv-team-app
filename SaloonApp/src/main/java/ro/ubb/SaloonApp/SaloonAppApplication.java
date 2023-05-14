b85f466bc2f58d997d6b12d5515487ca86f3e866
Stefan Bertici <stefan.bertici@gmail.com>
2023-05-14 12:33:13 +0300

    first commit

---
+++ SaloonApp/src/main/java/ro/ubb/SaloonApp/SaloonAppApplication.java
@@ -0,0 +1,13 @@
+package ro.ubb.SaloonApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaloonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaloonAppApplication.class, args);
+	}
+
+}
