64f7d81428abfb2e93c8e2be0620b44716a26bba
andrei-crisan <andreicrisan@protonmail.com>
2023-05-30 14:02:32 +0300

    Impl: DropDownList for chosing user(with employee role) when making the reservation

--- WebApp/src/app/pages/reservation/reservation-add/reservation-add.component.ts
+++ WebApp/src/app/pages/reservation/reservation-add/reservation-add.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Beauty } from 'src/app/model/beauty';
+import { User } from 'src/app/model/user';
 import { BeautyService } from 'src/app/service/beauty.service';
 import { IdentityService } from 'src/app/service/identity.service';
 import { ReservationService } from 'src/app/service/reservation.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-reservation-add',
@@ -13,23 +15,27 @@ import { ReservationService } from 'src/app/service/reservation.service';
 
 export class ReservationAddComponent implements OnInit {
   beautyServiceRecived: Beauty[] = [];
+  employeeList: User[] = [];
   currentDate: Date = new Date();
 
   constructor(private reservationService: ReservationService,
     private matDialog: MatDialogRef<ReservationAddComponent>,
-    private beautyService: BeautyService, private identityService: IdentityService) { }
-    
+    private beautyService: BeautyService, private useService: UserService,
+    private identityService: IdentityService) { }
+
   ngOnInit(): void {
     this.fetchAvailableServices();
+    this.fetchEmployees();
   }
 
   saveReservation(beautyServiceId: number, employeeId: number, date: string, hour: string) {
     let formattedDate: string = new Date(date).toLocaleDateString('fr-CA').toString(); //YYYY-MM-DD
     //Todo: ENUMS for hardcoded values;
-    let reservationToBeSaved = { customerId: this.identityService.loggedUser.id, 
-      beautyServiceId, employeeId, status: 'PENDING', date: formattedDate, hour };
+    let reservationToBeSaved = {
+      customerId: this.identityService.loggedUser.id,
+      beautyServiceId, employeeId, status: 'PENDING', date: formattedDate, hour
+    };
 
-    console.log(reservationToBeSaved);
     this.reservationService.save(reservationToBeSaved)
       .subscribe(_ => console.log("Reservation saved!"));
 
@@ -38,6 +44,12 @@ export class ReservationAddComponent implements OnInit {
   fetchAvailableServices() {
     return this.beautyService.getAll()
       .subscribe(x => this.beautyServiceRecived = x);
+  }
 
+  fetchEmployees() {
+    return this.useService.getAll()
+      .subscribe((x: User[]) => {
+        this.employeeList = x.filter((y: User) => y.role == 'EMPLOYEE');
+      });
   }
 }
