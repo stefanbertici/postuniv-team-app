a6e7ab682921461bdb4ec90011a3ddf15c9b53f0
andrei-crisan <andreicrisan@protonmail.com>
2023-06-05 12:03:16 +0300

    Impl: pwd_change Alert

--- WebApp/src/app/pages/user/user-pwd/user-pwd.component.ts
+++ WebApp/src/app/pages/user/user-pwd/user-pwd.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/model/user';
@@ -11,13 +12,29 @@ import { UserService } from 'src/app/service/user.service';
 })
 export class UserPwdComponent {
   selectedUser: User = this.data;
+  formGroup!: FormGroup;
 
   constructor(private userService: UserService, private matDialogRef: MatDialogRef<UserPwdComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
 
+  initForm() {
+    this.formGroup = new FormGroup({
+      pwd: new FormControl('', [Validators.required]),
+      pwdConfirm: new FormControl('', [Validators.required])
+    })
+
+  }
+
   changePwd(newPwd: string) {
     this.userService.updatePwd(this.selectedUser.id, newPwd)
-      .subscribe(_ => console.log("Pwd updated!"));
+      .subscribe({
+        next: () => { console.log("Pwd updated!") },
+        error: () => {
+          alert("Password changed!");
+          location.reload();
+        }
+      });
+
   }
 
   closeModalComponent() {
