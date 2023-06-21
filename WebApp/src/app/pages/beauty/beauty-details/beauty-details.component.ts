6c90fd4f6fec926879a58f45054ecee42833a3c0
andrei-crisan <andreicrisan@protonmail.com>
2023-06-21 23:25:17 +0300

    Fix: update services_drodown list added for categories

--- WebApp/src/app/pages/beauty/beauty-details/beauty-details.component.ts
+++ WebApp/src/app/pages/beauty/beauty-details/beauty-details.component.ts
@@ -1,18 +1,25 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Beauty } from 'src/app/model/beauty';
+import { Category } from 'src/app/model/category';
 import { BeautyService } from 'src/app/service/beauty.service';
+import { CategoryService } from 'src/app/service/category.service';
 
 @Component({
   selector: 'app-beauty-details',
   templateUrl: './beauty-details.component.html',
   styleUrls: ['./beauty-details.component.scss']
 })
-export class BeautyDetailsComponent {
+export class BeautyDetailsComponent implements OnInit{
   beautyServiceSelected: Beauty = this.data;
+  categories: Category[] = [];
+
+  ngOnInit(): void {
+    this.fetchAvailableCategories();
+  }
 
   constructor(private beautyService: BeautyService, private matDialogRef: MatDialogRef<BeautyDetailsComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+    @Inject(MAT_DIALOG_DATA) public data: any, private categoryService: CategoryService) { }
 
   updateBeautyService(name: string, numOfAvailabilityBlocks: number, price: number, categoryName: string) {
     let beautyServiceToBeUpdated = { id: this.beautyServiceSelected.id, name, numOfAvailabilityBlocks, price, categoryName };
@@ -26,4 +33,8 @@ export class BeautyDetailsComponent {
     this.matDialogRef.close();
   }
 
+  fetchAvailableCategories() {
+    return this.categoryService.getAll()
+      .subscribe(x => this.categories = x);
+  }
 }
