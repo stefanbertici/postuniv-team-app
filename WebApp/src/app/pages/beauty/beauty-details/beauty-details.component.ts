414a01a1970afca04166af5cc5975db7f484d5c1
andrei-crisan <andreicrisan@protonmail.com>
2023-06-22 10:23:52 +0300

    Fix/Impl: Dropdown list for picking time when updating a service

--- WebApp/src/app/pages/beauty/beauty-details/beauty-details.component.ts
+++ WebApp/src/app/pages/beauty/beauty-details/beauty-details.component.ts
@@ -1,7 +1,9 @@
+import { MapType } from '@angular/compiler';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Beauty } from 'src/app/model/beauty';
 import { Category } from 'src/app/model/category';
+import { TimeUnit } from 'src/app/model/timeunit';
 import { BeautyService } from 'src/app/service/beauty.service';
 import { CategoryService } from 'src/app/service/category.service';
 
@@ -13,15 +15,29 @@ import { CategoryService } from 'src/app/service/category.service';
 export class BeautyDetailsComponent implements OnInit{
   beautyServiceSelected: Beauty = this.data;
   categories: Category[] = [];
+  timeUnits: TimeUnit[] = [
+    { id: 1, displayValue: '30m', equivalentValue: 1 },
+    { id: 2, displayValue: '1h', equivalentValue: 2 },
+    { id: 3, displayValue: '1h30m', equivalentValue: 3 },
+    { id: 4, displayValue: '2h', equivalentValue: 4 },
+    { id: 5, displayValue: '2h30m', equivalentValue: 5 },
+    { id: 6, displayValue: '3h', equivalentValue: 6 },
+    { id: 7, displayValue: '3h30m', equivalentValue: 7 },
+    { id: 8, displayValue: '4h', equivalentValue: 8 },
+  ];
+
 
   ngOnInit(): void {
     this.fetchAvailableCategories();
   }
 
   constructor(private beautyService: BeautyService, private matDialogRef: MatDialogRef<BeautyDetailsComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any, private categoryService: CategoryService) { }
+    @Inject(MAT_DIALOG_DATA) public data: any, private categoryService: CategoryService) {
+
+     }
 
   updateBeautyService(name: string, numOfAvailabilityBlocks: number, price: number, categoryName: string) {
+   
     let beautyServiceToBeUpdated = { id: this.beautyServiceSelected.id, name, numOfAvailabilityBlocks, price, categoryName };
 
     this.beautyService.update(beautyServiceToBeUpdated)
